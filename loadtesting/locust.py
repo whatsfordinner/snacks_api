d2039a5e48dbb078335dc94fbabaec2d18d6e302
what's for dinner <cruelangeleo7@gmail.com>
2020-09-07 21:25:59 +1000

    got the loadtesting working, commented out logging

--- loadtesting/locust.py
+++ loadtesting/locust.py
@@ -17,25 +17,162 @@ class SnackdrawerUser(HttpUser):
     @task(10)
     def view_snacks(self):
         response = self.client.get('/snacks/')
-        self.snacks = json.loads(response.text)
+        snacks = json.loads(response.text)
+        self.snacks = snacks['snacks']
 
-    @task(1)
+    @task(4)
     def add_snack(self):
         if self.auth_token is None:
             self.login()
         
-        response = self.client.post(
-            '/snacks/',
-            json={
-                'name': get_random_string()
-            },
-            headers={
-                'x-access-token': self.auth_token
-            }
-        )
+        try:
+            response = self.client.post(
+                '/snacks/',
+                json={
+                    'name': get_random_string()
+                },
+                headers={
+                    'x-access-token': self.auth_token
+                }
+            )
 
-        if response.status_code == 401:
+            if response.status_code == 401:
+                raise ValueError
+
+        except ValueError:
             self.auth_token = None
+            self.add_snack()
+    
+    @task(3)
+    def view_drawers(self):
+        if self.auth_token is None:
+            self.login()
+
+        try:
+            response = self.client.get(
+                '/drawers/',
+                headers={
+                    'x-access-token': self.auth_token
+                }
+            )
+
+            if response.status_code == 401:
+                raise ValueError
+
+            drawers = json.loads(response.text)
+            self.drawers = drawers['drawers']
+        
+        except ValueError:
+            self.auth_token = None
+            self.view_drawers()
+
+    @task(2)
+    def view_drawer(self):
+        if self.auth_token is None:
+            self.login()
+
+        if self.drawers is None:
+            self.view_drawers()
+
+        if len(self.drawers) == 0:
+            self.create_drawer()
+            self.view_drawers()
+
+        try:
+            drawer = random.choice(self.drawers)['id']
+            response = self.client.get(
+                f'/drawers/{drawer}',
+                headers={
+                    'x-access-token': self.auth_token
+                }
+            )
+
+            if response.status_code == 401:
+                raise ValueError
+        
+        except ValueError:
+            self.auth_token = None
+            self.view_drawer()
+
+    @task(1)
+    def create_drawer(self):
+        if self.auth_token is None:
+            self.login()
+        
+        try:
+            drawer_name = get_random_string()
+            result = self.client.post(
+                '/drawers/',
+                json={
+                    'name': drawer_name
+                },
+                headers={
+                    'x-access-token': self.auth_token
+                }
+            )
+
+            if result.status_code == 401:
+                raise ValueError
+
+        except ValueError:
+            self.auth_token = None
+            self.create_drawer()
+
+    @task(3)
+    def add_snack_to_drawer(self):
+        if self.auth_token is None:
+            self.login()
+        
+        if self.drawers is None:
+            self.view_drawers()
+        
+        if len(self.drawers) == 0:
+            self.create_drawer()
+            self.view_drawers()
+
+        if self.snacks is None:
+            self.view_snacks()
+        
+        try:
+            drawer = random.choice(self.drawers)['id']
+            response = self.client.get(
+                f'/drawers/{drawer}',
+                headers={
+                    'x-access-token': self.auth_token
+                }
+            )
+
+            if response.status_code == 401:
+                raise ValueError
+            
+            contents = json.loads(response.text)
+
+            snack_id = None
+            for snack in self.snacks:
+                candidate = True
+                for content in contents['drawer']['contents']:
+                    if snack['id'] == content['id']:
+                        candidate = False
+                
+                if candidate == True:
+                    snack_id = snack['id']
+                    break
+
+            result = self.client.post(
+                f'/drawers/{drawer}',
+                json={
+                    'snack': snack_id
+                },
+                headers={
+                    'x-access-token': self.auth_token
+                }
+            )
+
+            if result.status_code == 401:
+                raise ValueError
+        except ValueError:
+            self.auth_token = None
+            self.add_snack_to_drawer()
 
     def login(self):
         if self.user_created is False:
@@ -65,5 +202,4 @@ class SnackdrawerUser(HttpUser):
 
 def get_random_string() -> str:
     letters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(letters) for i in range(10))
-    return password
+    return ''.join(random.choice(letters) for i in range(10))
--- snackdrawer/__init__.py
+++ snackdrawer/__init__.py
@@ -33,18 +33,19 @@ def create_app():
 
 # TODO: this doesn't seem to work right with access logging
 def configure_logging():
-    dictConfig({
-        'version': 1,
-        'formatters': {'default': {
-            'format': '%(asctime)s %(levelname)s [%(name)s] %(message)s',
-        }},
-        'handlers': {'wsgi': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://flask.logging.wsgi_errors_stream',
-            'formatter': 'default'
-        }},
-        'root': {
-            'level': 'INFO',
-            'handlers': ['wsgi']
-        }
-    })
+    pass
+    #dictConfig({
+        #'version': 1,
+        #'formatters': {'default': {
+            #'format': '%(asctime)s %(levelname)s [%(name)s] %(message)s',
+        #}},
+        #'handlers': {'wsgi': {
+            #'class': 'logging.StreamHandler',
+            #'stream': 'ext://flask.logging.wsgi_errors_stream',
+            #'formatter': 'default'
+        #}},
+        #'root': {
+            #'level': 'INFO',
+            #'handlers': ['wsgi']
+        #}
+    #})
